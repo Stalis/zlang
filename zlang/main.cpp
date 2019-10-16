730b683d1bdd8d4dd34a87fa730a0071c76322d2
Stanislaw Babkin <stalissss@outlook.com>
2019-10-16 09:02:56 +0300

    Initial Commit

---
+++ zlang/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zlang
+//
+//  Created by Stanislav on 16/10/2019.
+//  Copyright © 2019 Stanislav. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
